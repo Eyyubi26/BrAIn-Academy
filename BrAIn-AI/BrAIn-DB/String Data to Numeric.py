d83279bc6e32bc91c188dfac9d4b65063633b1ea
Eyüp <eyup.ipler@gmail.com>
2023-07-28 21:09:57 +0300

    String Data to Numeric.py

--- BrAIn-AI/BrAIn-DB/String Data to Numeric.py
+++ BrAIn-AI/BrAIn-DB/String Data to Numeric.py
@@ -1,3 +1,4 @@
+#WARNING! This code is a sample code that shows how the "String Data to Numeric" works.
 import os
 
 ones = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
