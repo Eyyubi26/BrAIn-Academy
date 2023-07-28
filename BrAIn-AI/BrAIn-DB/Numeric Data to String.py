3cbbc5fc0d264b471bdb45a0c0f187d3e6a2026f
Eyüp <eyup.ipler@gmail.com>
2023-07-28 21:05:58 +0300

    Add files via upload

---
+++ BrAIn-AI/BrAIn-DB/Numeric Data to String.py
@@ -0,0 +1,56 @@
+import os
+
+digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+hundreds = ["", "hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"]
+thousands = ["", "thousand", "million", "billion", "trillion"]
+
+def numericdata_to_string(number):
+    if number == 0:
+        return "zero"
+
+    text = ""
+    number_str = str(number)
+    digit_groups = (len(number_str) - 1) // 3
+
+    for group in range(digit_groups, -1, -1):
+        group_str = number_str[-3:]
+        number_str = number_str[:-3]
+
+        hundreds_digit = int(group_str) // 100
+        tens_digit = (int(group_str) % 100) // 10
+        ones_digit = int(group_str) % 10
+
+        if hundreds_digit > 0:
+            text += hundreds[hundreds_digit] + " "
+
+        if tens_digit > 0:
+            text += tens[tens_digit] + " "
+
+        if ones_digit > 0:
+            text += digits[ones_digit] + " "
+
+        if group > 0 and int(group_str) > 0:
+            text += thousands[group] + " "
+
+    return text.strip()
+
+def savedata(folder, text):
+    file_path = os.path.join(folder, "TextData.txt")
+    with open(file_path, "a") as file:
+        file.write(f"{text}\n")
+    print(f"Data ({text}) successfully saved.")
+
+folder = input("Enter the folder where data will be saved: ")
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+while True:
+    try:
+        number = int(input("Please enter a number: "))
+        text_representation = numericdata_to_string(number)
+        print(f"The number {number} is written as \"{text_representation}\".")
+        savedata(folder, f"{text_representation}")
+        break
+    except ValueError:
+        print("Incorrect input, please try again.")
---
+++ BrAIn-AI/BrAIn-DB/String Data to Numeric.py
@@ -0,0 +1,46 @@
+import os
+
+ones = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+tens = {"ten": 10, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90}
+hundreds = {"hundred": 100, "twohundred": 200, "threehundred": 300, "fourhundred": 400, "fivehundred": 500, "sixhundred": 600, "sevenhundred": 700, "eighthundred": 800, "ninehundred": 900}
+thousands = {"thousand": 1000, "million": 1000000, "billion": 1000000000, "trillion": 1000000000000}
+
+def stringdata_to_numeric(text):
+    if text == "zero":
+        return "0"
+
+    split_text = text.split()
+    number = 0
+    group_count = 0
+
+    for word in split_text:
+        if word in ones:
+            number += ones[word]
+        elif word in tens:
+            number += tens[word]
+        elif word in hundreds:
+            number += hundreds[word]
+        elif word in thousands:
+            number *= thousands[word]
+            group_count += 1
+
+    return number
+
+def savedata(folder, data, filename):
+    file_path = os.path.join(folder, filename)
+    with open(file_path, "a") as file:
+        file.write(f"{data}\n")
+    print(f"Data ({data}) successfully saved.")
+
+folder = input("Enter the folder where data will be saved: ")
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+while True:
+    text = input("Please enter some text: ")
+    try:
+        numeric_value = stringdata_to_numeric(text)
+        print(f"The text \"{text}\" is written as the number {numeric_value}.")
+        savedata(folder, f"{numeric_value}", "NumericData.txt")
+    except KeyError:
+        print("Incorrect text input. Please check the text and try again.")
